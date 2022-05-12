ec97a6469784819d96471447f6c95c004d05b38c
Jurki <natsuki.rin111@gmail.com>
2022-05-12 16:53:49 +0200

    merging and def some var in css

--- script.js
+++ script.js
@@ -3,29 +3,18 @@ fetch("music.json") //Carga en memoria
 .then(res=>res.json()) //Especifica el formato en que se desea obtener la información, que es igual a hace un JSON.parse()
 .then(data => musicList(data)) //Mostrar la información
 
-const body= document.querySelector("body");
-const musicName= [];
+const main= document.querySelector("main");
+const titleList= document.createElement("h2");
+const numberedList= document.createElement("ol");
+main.appendChild(titleList);
+titleList.appendChild(numberedList);
 
-
+window.addEventListener("load", ...);
 
 function musicList(data){
     for (let i = 0; i < data.length; i++) {
-        // ({name:data.name}.{})
         
-        //Título
-        //lista que este relacionada con
-    }
+        // ({name:data.name}.{})
 
-    
-   
+    }   
 }
-
- /*const linkList=document.querySelectorAll("a");
-    linkList[1].setAttribute('href', "data.name.");
-     linkList[2].setAttribute('href', "");
-    linkList[3].setAttribute('href', "");
-    linkList[4].setAttribute('href', "");
-    linkList[5].setAttribute('href', "");
-    linkList[6].setAttribute('href', "");
-    linkList[7].setAttribute('href', "");
-    linkList[8].setAttribute('href', "");*/
