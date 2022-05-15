23abddd3cec268321d940367341c308193389a8e
Jurki <natsuki.rin111@gmail.com>
2022-05-16 00:59:24 +0200

    more css

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ main.appendChild(listContainer);
 listContainer.appendChild(titleList);
 
 
+
     fetch("music.json") //Carga en memoria
     .then(res => res.json()) //Estdecifica el formato en que se desea obtener la información, que es igual a hacer un JSON.tdarse()
     .then(data => musicList(data)) //Mostrar la información obtenida
@@ -16,9 +17,9 @@ function musicList(songs){
     songs.forEach(song => { 
         const list1= document.createElement("tr");
         list1.innerHTML += `
-        <td><a href= "${song.artist.url}">${song.artist.name}</a></td>
+        <td class="nameArtist"><a href= "${song.artist.url}">${song.artist.name}</a></td>
         <td><a href="${song.url}" target="_blank">${song.name}</a></td> 
-        <td>${song.listeners} Listeners</td>    
+        <td class= "listeners">${song.listeners} Listeners</td>    
         `;
         listContainer.appendChild(list1);
     });  
